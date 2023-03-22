9729384dc1ee658f2d2d39802b6e218809050a46
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-03-22 13:53:56 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.  Ogni membro è caratterizzato dalle seguenti informazioni:
+nome
+ruolo
+foto
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede */
+
+const teamMembers = [
+    {
+        name: 'Ratallo',
+        role: 'God aimer',
+        photo: '',
+    },
+    {
+        name: 'Alegandro',
+        role: 'Almost god aimer',
+        photo: '',
+    },
+    {
+        name: 'Lookas',
+        role: 'Bot a pedali',
+        photo: '',
+    },
+    {
+        name: 'Kolen',
+        role: 'Stella nascente',
+        photo: '',
+    },
+    {
+        name: 'Paolino',
+        role: 'Motivatore',
+        photo: '',
+    },
+    {
+        name: 'Giamma',
+        role: 'Bot senza pedali',
+        photo: '',
+    },
+]
+
+console.log(teamMembers);
